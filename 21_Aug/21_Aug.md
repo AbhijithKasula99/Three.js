b892dfed0ff5116c3c7319bc549f4364709e9265
AbhijithKasula99 <abhijithkasula99@gmail.com>
2023-08-21 19:07:21 -0400

    Resolved the error in code

--- 21_Aug/21_Aug.md
+++ 21_Aug/21_Aug.md
@@ -159,35 +159,37 @@ HTML Code
 ```
 JS Code
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+// Creating a Scene
+const scene = new THREE.Scene();
 
-// Sizes
+// Creating an object
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:'#fefae0'})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Aspect Ratio
 const sizes = {
     width: 800,
     height: 600
 }
 
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: '#ff0000'
-})
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-scene.add(cubeMesh)
-
-// Camera
+// Defining the Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-// Renderer
+// We import canvas using DOM method and add it to the renderer
+const canvas = document.querySelector('canvas.webgl')
+
+// Defining the Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+   canvas: canvas
+});
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//Render the setup
+renderer.render(scene, camera);
+
+
 ```
--- 21_Aug/script.js
+++ 21_Aug/script.js
@@ -1,65 +1,32 @@
 // Creating a Scene
-//const scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
 // Creating an object
-//const geometry = new THREE.BoxGeometry(1,1,1)
-//const material = new THREE.MeshBasicMaterial({color:'#fefae0'})
-//const mesh = new THREE.Mesh(geometry, material)
-//const cubeMesh = new THREE.Mesh(geometry, materialaterial)
-//scene.add(cubeMesh)
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:'#fefae0'})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 // Aspect Ratio
-//const sizes = {
-    //width: 800,
-  //  height: 600
-//}
-
-// Defining the Camera
-//const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-//camera.position.z = 10
-//camera.position.x = 3
-//scene.add(camera)
-
-// We import canvas using DOM method and add it to the renderer
-// const canvas = document.querySelector('canvas.webgl')
-
-// Defining the Renderer
-//const renderer = new THREE.WebGLRenderer({
-   // canvas: canvas
-// });
-// renderer.setSize(sizes.width, sizes.height)
-
-// Render the setup 
-// renderer.render(scene, camera);
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: '#ff0000'
-})
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-scene.add(cubeMesh)
-
-// Camera
+// Defining the Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-// Renderer
+// We import canvas using DOM method and add it to the renderer
+const canvas = document.querySelector('canvas.webgl')
+
+// Defining the Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+   canvas: canvas
+});
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//Render the setup 
+renderer.render(scene, camera);
+
